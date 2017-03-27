78af06c758ab5289fc9590b78f278971cd2eba75
kdada <me@imkira.com>
2017-03-27 22:15:26 +0800

    update routes

--- cmd/server/stage/ipam.go
+++ cmd/server/stage/ipam.go
@@ -1,9 +1,14 @@
 package stage
 
 import (
+	"bufio"
+	"bytes"
+	"io/ioutil"
 	"log"
 	"net"
 	"os"
+	"strconv"
+	"strings"
 
 	"github.com/kdada/tinyvpn/pkg/ipam"
 	"github.com/kdada/tinyvpn/pkg/proto"
@@ -39,7 +44,30 @@ func init() {
 	r := [5]byte{}
 	r[4] = 32
 	copy(r[:], ip)
-	Routes = [][5]byte{r}
+	Routes = make([][5]byte, 0, 10000)
+	Routes = append(Routes, r)
+	data, err := ioutil.ReadFile("./out.txt")
+	if err != nil {
+		log.Println(err)
+		os.Exit(-1)
+	}
+	reader := bufio.NewReader(bytes.NewReader(data))
+	for true {
+		l, err := reader.ReadString('\n')
+		if err != nil {
+			break
+		}
+		arrs := strings.Split(l, "/")
+		rip := net.ParseIP(arrs[0]).To4()
+		x := arrs[1]
+		m, err := strconv.Atoi(x[:len(x)-1])
+
+		r = [5]byte{}
+		copy(r[:], rip)
+		r[4] = byte(m)
+		Routes = append(Routes, r)
+	}
+	log.Println("routes:", len(Routes))
 
 	ips = &net.IPNet{
 		IP:   ip,
